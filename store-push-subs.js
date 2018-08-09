d2f9a0e396b539f3c9bbe5032ff3b7f0a413623d
Richard Kotze <rkotze@findmypast.com>
2018-08-09 19:08:00 +0100

    Create a subs folder if it does not exist

--- store-push-subs.js
+++ store-push-subs.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const SUB_PATH = "subs/";
 
+const mkdirSync = function(dirPath) {
+  try {
+    fs.mkdirSync(dirPath);
+  } catch (err) {
+    if (err.code !== "EEXIST") throw err;
+  }
+};
+
 function write(subName, pushMessageJSON) {
+  mkdirSync(SUB_PATH);
   return new Promise((resolve, reject) => {
     const jsonString = JSON.stringify(pushMessageJSON);
-    fs.writeFile(SUB_PATH + subName, jsonString, { flag: 'w' }, err => {
+    fs.writeFile(SUB_PATH + subName, jsonString, { flag: "w" }, err => {
       if (err) reject(err);
 
       resolve();
@@ -15,7 +24,7 @@ function write(subName, pushMessageJSON) {
 
 function read(subName) {
   return new Promise((resolve, reject) => {
-    fs.readFile(SUB_PATH + subName, 'utf8', (err, data) => {
+    fs.readFile(SUB_PATH + subName, "utf8", (err, data) => {
       if (err) reject(err);
 
       resolve(data);
