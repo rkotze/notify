44dc05885cc5d25671b8af6ab54bf60636fb1d98
Richard Kotze <rkotze@findmypast.com>
2018-08-13 21:07:01 +0100

    Format and add ejs to node js views

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const webpush = require('web-push');
-const config = require('config');
-const subs = require('./store-push-subs');
-const users = require('./src/users.sql');
+const express = require("express");
+const webpush = require("web-push");
+const config = require("config");
+const subs = require("./store-push-subs");
+const users = require("./src/users.sql");
 
 function getAppKeys() {
-  try{
-    const webPush = config.get('webPush');
-    return { 
+  try {
+    const webPush = config.get("webPush");
+    return {
       publicVapidKey: webPush.publicKey,
       privateVapidKey: webPush.privateKey
     };
-  }catch(e){
-    return { 
+  } catch (e) {
+    return {
       publicVapidKey: process.env.PUBLIC_VAPID_KEY,
       privateVapidKey: process.env.PRIVATE_VAPID_KEY
     };
@@ -21,29 +21,46 @@ function getAppKeys() {
 
 const { publicVapidKey, privateVapidKey } = getAppKeys();
 
-webpush.setVapidDetails('mailto:fake@newexample.com', publicVapidKey, privateVapidKey);
+webpush.setVapidDetails(
+  "mailto:fake@newexample.com",
+  publicVapidKey,
+  privateVapidKey
+);
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(require('body-parser').json());
+app.use(express.static("public"));
+app.use(require("body-parser").json());
+app.set("view engine", "ejs");
 
-app.get('/web-push/setup', (req, res) => {
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/web-push/setup", (req, res) => {
   res.status(200).json({
-    "vapidPublicKey": publicVapidKey
+    vapidPublicKey: publicVapidKey
   });
 });
 
-app.get('/web-push/users/get-all', async (req, res) => {
+app.get("/web-push/users/get-all", async (req, res) => {
   const dbUsers = await users.getAll();
   res.status(200).json(dbUsers.rows);
 });
 
-app.post('/web-push/users/subscribe', async (req, res) => {
+app.post("/web-push/users/subscribe", async (req, res) => {
   const user = req.body;
   if (user.name) {
     const { endpoint, expirationTime, keys } = user.subscription;
-    await users.insert([user.name, user.category, user.granted, endpoint, expirationTime, keys.p256dh, keys.auth]);
+    await users.insert([
+      user.name,
+      user.category,
+      user.granted,
+      endpoint,
+      expirationTime,
+      keys.p256dh,
+      keys.auth
+    ]);
     res.status(201).end();
   } else {
     res.status(400).json({
@@ -52,13 +69,16 @@ app.post('/web-push/users/subscribe', async (req, res) => {
   }
 });
 
-app.get('/web-push/send/:username', async (req, res) => {
-
+app.get("/web-push/send/:username", async (req, res) => {
   const username = req.params.username;
   try {
     const subscriptionStr = await subs.read(username);
     const subscription = JSON.parse(subscriptionStr);
-    const payload = JSON.stringify({ title: "Amazing", body: `Check this content ${username}`, icon: "/icon.png" });
+    const payload = JSON.stringify({
+      title: "Amazing",
+      body: `Check this content ${username}`,
+      icon: "/icon.png"
+    });
 
     webpush.sendNotification(subscription, payload).catch(error => {
       console.error(error.stack);
@@ -67,12 +87,12 @@ app.get('/web-push/send/:username', async (req, res) => {
   } catch (ex) {
     if (ex.code === "ENOENT") {
       return res.status(400).json({
-        "error": "Username not found"
+        error: "Username not found"
       });
     }
 
     res.status(500).json({
-      "error": "Oops ... something went wrong!!"
+      error: "Oops ... something went wrong!!"
     });
   }
 });
