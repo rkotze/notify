979414eaab24ff1ac8752fdd85ec30d5f14e6282
Richard Kotze <rkotze@findmypast.com>
2018-08-13 18:02:33 +0100

    Handle queries to users table

---
+++ src/users.sql.js
@@ -0,0 +1,33 @@
+const dbClient = require('./dbClient');
+
+export async function insert() {
+  const client = dbClient();
+  await client.connect();
+
+  const insertQuery = `INSERT INTO users( name, category, granted, endpoint, expirationTime, keyp256dh, keyAuth) 
+    VALUES($1, $2, $3, $4, $5, $6, $7)`;
+  const values = [
+    "brianc",
+    "sub",
+    true,
+    "AKSFEW342ks",
+    "1999-01-08 04:05:06",
+    "asdf8we",
+    "aeiieiwwe883"
+  ];
+
+  const res = await client.query(insertQuery, values);
+  await client.end();
+  return res;
+}
+
+export async function getAllUsers() {
+  const client = dbClient();
+  await client.connect();
+
+  const text = `SELECT * FROM users`;
+
+  const res = await client.query(text);
+  await client.end();
+  return res;
+}
